9383b926d17df0548e940fa7fe9f4d616a851f41
Daniel PC <danielmoliaribarbosa@gmail.com>
2024-06-29 19:02:18 -0300

    feat: Implemented all methods of city for API

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -9,19 +9,11 @@ interface ICidade {
     estado: string;
 }
 
-interface IFilter {
-    filter?: string;
-}
-
-
 export const createValidation = validation((getSchema) => ({
     body: getSchema<ICidade>(yup.object().shape({
         nome: yup.string().required().min(3),
         estado: yup.string().required().min(3),
     })),
-    query: getSchema<IFilter>(yup.object().shape({
-        filter: yup.string().optional().min(3),
-    }))
 }));
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
