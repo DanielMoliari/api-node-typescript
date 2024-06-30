7d677f1692f67634e9672175a30ded0ec59a97cd
Daniel PC <danielmoliaribarbosa@gmail.com>
2024-06-30 20:59:29 -0300

    feat: all methods and yup tratments for node api project

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -6,18 +6,15 @@ import { validation } from '../../shared/middleware'
 
 interface ICidade {
     nome: string;
-    estado: string;
 }
 
 export const createValidation = validation((getSchema) => ({
     body: getSchema<ICidade>(yup.object().shape({
         nome: yup.string().required().min(3),
-        estado: yup.string().required().min(3),
     })),
 }));
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-    console.log(req.body);
-    
-    return res.send('Create!');
-}
+
+    return res.status(StatusCodes.CREATED).json(1);
+  };
