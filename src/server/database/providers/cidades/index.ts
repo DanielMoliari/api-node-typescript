7219492b925a0abff649a651f74aa805ac1ac4a1
Daniel PC <danielmoliaribarbosa@gmail.com>
2024-07-05 23:41:48 -0300

    fix: refact the others controllers methods for cidades

--- src/server/database/providers/cidades/index.ts
+++ src/server/database/providers/cidades/index.ts
@@ -3,6 +3,7 @@ import * as updateById from './UpdateById';
 import * as getById from './GetById';
 import * as create from './Create';
 import * as getAll from './GetAll';
+import * as count from './Count';
 
 
 export const CidadesProvider = {
@@ -11,4 +12,5 @@ export const CidadesProvider = {
   ...getById,
   ...create,
   ...getAll,
+  ...count,
 };
