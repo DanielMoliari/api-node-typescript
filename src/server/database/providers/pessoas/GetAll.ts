a3e34c1036c052aa42e1e9c33ffe6b9a1a706efe
Daniel PC <danielmoliaribarbosa@gmail.com>
2024-07-07 19:14:35 -0300

    feat: controllers of pessoas and outes implementeds

--- src/server/database/providers/pessoas/GetAll.ts
+++ src/server/database/providers/pessoas/GetAll.ts
@@ -3,18 +3,17 @@ import { IPessoa } from '../../models';
 import { Knex } from '../../knex';
 
 
-export const getById = async (id: number): Promise<IPessoa | Error> => {
+export const getAll = async (page: number, limit: number, filter: string): Promise<IPessoa[] | Error> => {
   try {
     const result = await Knex(ETableNames.pessoa)
       .select('*')
-      .where('id', '=', id)
-      .first();
+      .where('nomeCompleto', 'like', `%${filter}%`)
+      .offset((page - 1) * limit)
+      .limit(limit);
 
-    if (result) return result;
-
-    return new Error('Registro não encontrado');
+    return result;
   } catch (error) {
     console.log(error);
-    return new Error('Erro ao consultar o registro');
+    return new Error('Erro ao consultar os registros');
   }
 };
