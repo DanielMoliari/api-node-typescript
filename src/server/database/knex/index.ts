f691cdc66ab4d4b5fec4b63cdc884135f8c7cb14
Moliari <danielmoliaribarbosa@gmail.com>
2024-07-10 19:55:12 -0300

    feat: adition of postgress and fix of controller getAll of cidade

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,14 +1,21 @@
-import knex from "knex";
+import { knex } from 'knex';
+import 'dotenv/config';
+import pg from 'pg';
+
 import { development, production, test } from './Environment';
 
-const getEnvironment = () => {
-    switch (process.env.NODE_ENV) {
-        case 'production': return production;
-        case 'test': return test;
 
-        default: return development;
-    }
+if (process.env.NODE_ENV === 'production') {
+  pg.types.setTypeParser(20, 'text', parseInt);
 }
 
+const getEnvironment = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production': return production;
+    case 'test': return test;
+
+    default: return development;
+  }
+};
 
 export const Knex = knex(getEnvironment());
